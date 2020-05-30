02576787f5fb50ce1aa4ae85f3fa30b50419a595
Georgia <lgeorgia95@gmail.com>
2020-05-30 22:02:53 +0300

    reservation services

---
+++ reservation-services/src/main/java/com/georgialamni/springcloud/ReservationWebServices.java
@@ -0,0 +1,28 @@
+package com.georgialamni.springcloud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationWebServices {
+
+    private ReservationRepository repository;
+
+    public ReservationWebServices(ReservationRepository repository){
+        super();
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public Iterable<Reservation> getAllReservations(){
+        return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Reservation getReservation(@PathVariable("id") long id){
+        return this.repository.findById(id).get();
+    }
+}
